71f3d8245382abbb51301b7dba5407b0e9cf116a
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2025-02-10 08:18:13 -0500

    basic data types

---
+++ module-1/src/1.4.ts
@@ -0,0 +1,31 @@
+// basic data type
+// str
+let firstName: string = "Mezbah"
+
+
+// number
+let number: number = 12344
+
+// boolean
+let isAdmin : boolean = true
+
+
+// undefined
+let x: undefined = undefined
+
+// null
+let y: null = null
+
+
+// any
+let d;
+
+
+// Array
+let friends : string[] = ["mejba", "tarek"];
+let elligableNumber: number[] = [1, 6 ]
+elligableNumber.push(77, 76)
+
+// tuple
+let coordinates: [number, string] = [32, "Mejba"];
+coordinates[0] = 4
