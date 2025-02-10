979df2365170c41cdecd9bb2cdd0f5afc87f6fa7
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2025-02-10 08:29:02 -0500

    Basic Array type

---
+++ module-1/src/1.5.ts
@@ -0,0 +1,15 @@
+// reference type --> object
+
+const user: {
+    company: "Programming Hero"; //type --> literal types
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+    isMarried: boolean;
+} = {
+    company: "Programming Hero",
+    firstName: "Mejba",
+     middleName: "Ur",
+    lastName: "Rahman",
+    isMarried: true
+}
