463b724c368c37d431f9d6c66739ca72d4b7373e
mdmohiuddin10 <hmmohiuddinctg1997@gmail.com>
2025-02-11 07:43:09 -0500

    learning function

---
+++ module-1/src/1.6.ts
@@ -0,0 +1,26 @@
+// learn function
+// normal function
+// Arrow function
+
+function add(num1 : number, num2 : string) {
+    return num1 + num2;
+}
+
+add(2, "r")
+
+
+const addArrow = (num1: number, num2: number): number => num1 + num2;
+
+//  object --> function --> method
+
+const poorUser = {
+    name: "Mejbah",
+    balance : 0,
+    addBalance(balance: number): string {
+        return `My new balance is : ${this.balance + balance}`;
+    }
+}
+
+const arr: number[] = [1, 3, 5, 7];
+const newArr: number[]= arr.map((elem: number): number => elem + 2)
+
